d08cc59575e9c5a5557dcd4c117e942747eeccea
ChoWonmin <0108257@gmail.com>
2019-01-04 06:59:09 +0900

    chage Home

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import StackVis from './views/StackVis.';
 
 Vue.use(Router)
@@ -12,15 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: StackVis
     },
     {
       path: '/stackVis',
